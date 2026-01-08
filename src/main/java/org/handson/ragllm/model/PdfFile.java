beb74d18d333e27036fdc8d3abaaf1a304660688
Omer-DN <omerdayan94@gmail.com>
2026-01-08 19:42:49 +0200

    add PdfFile JPA entity for storing PDFs

---
+++ src/main/java/org/handson/ragllm/model/PdfFile.java
@@ -0,0 +1,36 @@
+package org.handson.ragllm.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pdf_files")
+public class PdfFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "filename", nullable = false)
+    private String filename;
+
+    @Lob
+    @Column(name = "data", nullable = false)
+    private byte[] data;
+
+    @Column(name = "uploaded_at", nullable = false)
+    private LocalDateTime uploadedAt;
+
+    protected PdfFile() {}
+
+    public PdfFile(String filename, byte[] data, LocalDateTime uploadedAt) {
+        this.filename = filename;
+        this.data = data;
+        this.uploadedAt = uploadedAt;
+    }
+
+    public Long getId() { return id; }
+    public String getFilename() { return filename; }
+    public byte[] getData() { return data; }
+    public LocalDateTime getUploadedAt() { return uploadedAt; }
+}
